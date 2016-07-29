8569cbd0ee11e54891a0f8462e285976f7785cde
Andrew Lobban <andrewglobban@gmail.com>
2016-07-29 08:38:37 -0500

    first commit

---
+++ spec/calculator.spec.js
@@ -0,0 +1,9 @@
+/**
+ * Created by vagrant on 7/29/16.
+ */
+
+describe('calculator', function() {
+    it('should add two numbers', function() {
+        expect(add(1,2)).toBe(3);
+    });
+});
---
+++ src/calculator.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vagrant on 7/29/16.
+ */
+
+function add (x, y) {
+    return x + y;
+}
