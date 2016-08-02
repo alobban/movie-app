86a39c17c6efab060522b1e5ec2d390aee9dcaf4
Andrew Lobban <andrewglobban@gmail.com>
2016-08-02 15:50:22 -0500

    Extract movieApp module

---
+++ src/movie-app/app.js
@@ -0,0 +1,5 @@
+/**
+ * Created by vagrant on 8/2/16.
+ */
+
+angular.module('movieApp', []);
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -2,7 +2,7 @@
  * Created by vagrant on 8/2/16.
  */
 
-angular.module('movieApp', [])
+angular.module('movieApp')
     .controller('SearchController', function($location) {
         var vm = this;
         vm.search = function() {
