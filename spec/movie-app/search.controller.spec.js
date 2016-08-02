1db909b82094eb713186978d713fb6fe5e71bd89
Andrew Lobban <andrewglobban@gmail.com>
2016-08-02 15:05:32 -0500

    Refactor and inject ngMock services

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -6,27 +6,31 @@ describe('Search Controller', function() {
     var $scope;
     var $location;
         
-    beforeEach(function() {
-        $location = {
-            url: ''
-        };
+    beforeEach(inject(function(_$controller_, _$location_) {
         $scope = {};
-        $scope.search = function() {
-            if ($scope.query) {
-                $location.url = '/results?q=star%20wars';
-            }
+        $location = _$location_;
+        
+        var fn = function($scope) {
+            
+            $scope.search = function() {
+                if ($scope.query) {
+                    $location.path('/results').search('q', $scope.query);
+                }
+            };
         };
-    });
+        
+        _$controller_(fn, { $scope: $scope, $location: $location });
+    }));
         
     it('redirects to the query results page for non-empty query', function() {
         $scope.query = 'star wars';
         $scope.search();
-        expect($location.url).toBe('/results?q=star%20wars');
+        expect($location.url()).toBe('/results?q=star%20wars');
     });
     
     it('does not redirect to query results for empty query', function() {
         $scope.query = '';
         $scope.search();
-        expect($location.url).toBe('');
+        expect($location.url()).toBe('');
     });
 });
