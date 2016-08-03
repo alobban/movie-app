6cdc783383e320fcb349bd9bd8efc195b222eb07
Andrew Lobban <andrewglobban@gmail.com>
2016-08-03 15:50:50 -0500

    Hard coded PMs since no code for PMs endpoint

--- src/movie-app/home.controller.js
+++ src/movie-app/home.controller.js
@@ -14,15 +14,16 @@ angular.module('movieApp')
         };
         
         // Get PopularMovies List
-        PopularMovies.get()
-            .then(function(data) {
+        // PopularMovies.get()
+        //     .then(function(data) {
+        var data = ['tt0076759', 'tt0080684', 'tt0086190'];
                 results = data;
                 findMovie(results[0]);
                 $interval(function() {
                     ++idx;
                     findMovie(results[idx % results.length]);
                 }, 5000);
-            });
+            // });
         $scope.result = results[0];
         
     });
