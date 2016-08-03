f30845a7273be89797a04f7f6be992c473e95868
Andrew Lobban <andrewglobban@gmail.com>
2016-08-03 11:16:05 -0500

    Add

--- spec/movie-app/search.controller.spec.js
+++ spec/movie-app/search.controller.spec.js
@@ -6,24 +6,48 @@ describe('Search Controller', function() {
 
     var $location;
     var $scope;
+    var $timeout;
 
     beforeEach(module('movieApp'));
 
-    beforeEach(inject(function(_$controller_, _$location_) {
+    beforeEach(inject(function(_$controller_, _$location_, _$timeout_) {
         $scope = {};
         $location = _$location_;
-        _$controller_('SearchController', { $scope: $scope, $location: _$location_ });
+        $timeout = _$timeout_;
+        _$controller_('SearchController', { $scope: $scope, $location: _$location_, $timeout: _$timeout_ });
     }));
 
-    it('should redirect to query results for non-empty query', function() {
+    it('redirects to query results for non-empty query', function() {
         $scope.query = 'star wars';
         $scope.search();
         expect($location.url()).toBe('/results?q=star%20wars');
     });
 
-    it('should not redirect to query results for empty query', function() {
+    it('does not redirect to query results for empty query', function() {
         $scope.query = '';
         $scope.search();
         expect($location.url()).toBe('');
+    });
+
+    it('redirects after 1 second of keyboard inactivity', function() {
+        $scope.query = 'star wars';
+        $scope.keyup();
+        $timeout.flush();
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
+        expect($location.url()).toBe('/results?q=star%20wars');
+    });
+    
+    it('cancels timeout in keydown', function () {
+        $scope.query = 'star wars';
+        $scope.keyup();
+        $scope.keydown();
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
+    });
+    
+    it('cancels timeout on search', function () {
+        $scope.query = 'star wars';
+        $scope.keyup();
+        $scope.search();
+        expect($timeout.verifyNoPendingTasks).not.toThrow();
     });
 });
--- src/movie-app/search.controller.js
+++ src/movie-app/search.controller.js
@@ -3,9 +3,18 @@
  */
 
 angular.module('movieApp')
-    .controller('SearchController', function($location, $scope) {
-        // var vm = this;
+    .controller('SearchController', function($location, $scope, $timeout) {
+        var timeout;
+        $scope.keyup = function() {
+            timeout = $timeout($scope.search, 1000);
+        };
+        
+        $scope.keydown = function() {
+            $timeout.cancel(timeout);
+        };
+        
         $scope.search = function() {
+            $timeout.cancel(timeout);
             if ($scope.query) {
                 $location.path('/results').search('q', $scope.query);
             }
