36dae342c750adb5ab5e6b0bc4efaa98ab3310e7
Andrew Lobban <andrewglobban@gmail.com>
2016-08-03 09:24:44 -0500

    Add markup and include UI-Bootstrap

--- src/movie-app/app.js
+++ src/movie-app/app.js
@@ -2,4 +2,14 @@
  * Created by vagrant on 8/2/16.
  */
 
-angular.module('movieApp', []);
+angular.module('movieApp', ['ui.bootstrap', 'ngRoute', 'omdb'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/results', {
+                templateUrl: 'movie-app/results.html',
+                controller: 'ResultsController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
