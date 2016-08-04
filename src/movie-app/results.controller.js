c5ae96f31c27a4f2960564e55c8aec3ed7c1d24a
Andrew Lobban <andrewglobban@gmail.com>
2016-08-04 18:31:54 -0500

    Expand accordion with movie result data

--- src/movie-app/results.controller.js
+++ src/movie-app/results.controller.js
@@ -11,5 +11,16 @@ angular.module('movieApp')
             })
             .catch(function() {
                 $scope.errorMessage = 'Something went wrong!';
-            });
+            })
+        ;
+        
+        $scope.expand = function expand(index, id) {
+            omdbApi.find(id)
+                .then(function(data) {
+                    console.log(data);
+                    $scope.results[index].data = data;
+                    $scope.results[index].open = true;
+                })
+            ;
+        }
     });
