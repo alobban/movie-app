29bbf21333ab8fc4584a6b114256243f2d20b630
Andrew Lobban <andrewglobban@gmail.com>
2016-08-04 20:32:41 -0500

    Add exception hanlding for Popular Movies carousel

--- spec/movie-app/home.controller.spec.js
+++ spec/movie-app/home.controller.spec.js
@@ -20,15 +20,17 @@ describe('Home Controller', function() {
     ];
     var $scope;
     var $interval;
+    var $q;
+    var $controller;
+    var $rootScope;
+    var omdbApi;
+    var PopularMovies;
+    var $exceptionHandler;
     
     beforeEach(module('movieApp'));
-    
-    beforeEach(inject(function(_$q_, _PopularMovies_) {
-        spyOn(_PopularMovies_, 'get').and.callFake(function() {
-            var deferred = _$q_.defer();
-            deferred.resolve(['tt0076759', 'tt0080684', 'tt0086190']);
-            return deferred.promise;
-        });
+
+    beforeEach(module(function($exceptionHandlerProvider) {
+        $exceptionHandlerProvider.mode('log');
     }));
     
     beforeEach(inject(function(_$q_, _omdbApi_) {
@@ -41,6 +43,8 @@ describe('Home Controller', function() {
                 deferred.resolve(results[1]);
             } else if (args === 'tt0086190') {
                 deferred.resolve(results[2]);
+            } else if (args === 'ttError') {
+                deferred.reject('error finding movie');
             } else {
                 deferred.reject();
             }
@@ -48,19 +52,33 @@ describe('Home Controller', function() {
         });
     }));
     
-    beforeEach(inject(function(_$controller_, _$interval_, _omdbApi_, _PopularMovies_, _$rootScope_) {
+    beforeEach(inject(function(_$controller_, _$interval_, _$q_, _omdbApi_, _PopularMovies_, _$rootScope_, _$exceptionHandler_) {
         $scope = {};
         $interval = _$interval_;
-        _$controller_('HomeController', {
-            $scope: $scope,
-            $interval: _$interval_,
-            omdbApi: _omdbApi_,
-            PopularMovies: _PopularMovies_
-        });
-        _$rootScope_.$apply();
+        $q = _$q_;
+        $controller = _$controller_;
+        $rootScope = _$rootScope_;
+        omdbApi = _omdbApi_;
+        PopularMovies = _PopularMovies_;
+        $exceptionHandler = _$exceptionHandler_;
     }));
     
     it('rotates movies every 5 seconds', function() {
+
+        spyOn(PopularMovies, 'get').and.callFake(function() {
+            var deferred = $q.defer();
+            deferred.resolve(['tt0076759', 'tt0080684', 'tt0086190']);
+            return deferred.promise;
+        });
+        
+        $controller('HomeController', {
+            $scope: $scope,
+            $interval: $interval,
+            omdbApi: omdbApi,
+            PopularMovies: PopularMovies
+        });
+        $rootScope.$apply();
+        
         // should have a default movie
         expect($scope.result.Title).toBe(results[0].Title);
         // should update after 5 seconds
@@ -72,5 +90,35 @@ describe('Home Controller', function() {
         // should return to default
         $interval.flush(5000);
         expect($scope.result.Title).toBe(results[0].Title);
+    });
+    
+    it('handles error', function() {
+
+        spyOn(PopularMovies, 'get').and.callFake(function() {
+            var deferred = $q.defer();
+            deferred.resolve(['tt0076759', 'tt0080684', 'tt0086190', 'ttError']);
+            return deferred.promise;
+        });
+
+        $controller('HomeController', {
+            $scope: $scope,
+            $interval: $interval,
+            omdbApi: omdbApi,
+            PopularMovies: PopularMovies
+        });
+        $rootScope.$apply();
+        
+        // should have a default movie
+        expect($scope.result.Title).toBe(results[0].Title);
+        // should update after 5 seconds
+        $interval.flush(5000);
+        expect($scope.result.Title).toBe(results[1].Title);
+        // should update after 5 seconds
+        $interval.flush(5000);
+        expect($scope.result.Title).toBe(results[2].Title);
+        // should return to default
+        $interval.flush(5000);
+        
+        expect($exceptionHandler.errors).toEqual(['error finding movie'])
     });
 });
